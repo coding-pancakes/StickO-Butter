02391852437ff559fdc8bac77fea9b2e3f60d7ec
giannag27 <giacosey28@gmail.com>
2025-07-15 11:23:20 -0400

    fixed nesting issue

--- butter-square/butterbot.py
+++ butter-square/butterbot.py
@@ -13,6 +13,12 @@ def get_meme():
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
+    
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if message.content.startswith('hello ^.^'):
+            await message.channel.send('Hello World!')
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -22,9 +28,3 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = MyClient(intents=intents)
 client.run(TOKEN)
-
-async def on_message(self, message):
-    if message.author == self.user:
-        return
-    if message.content.startswith('hello ^.^'):
-        await message.channel.send('Hello World!')
