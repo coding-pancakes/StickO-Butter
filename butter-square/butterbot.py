1dcd6996bd0825b9de0fb55d170392b947fbe7d7
giannag27 <giacosey28@gmail.com>
2025-07-14 22:37:13 -0400

    Set intents

--- butter-square/butterbot.py
+++ butter-square/butterbot.py
@@ -4,3 +4,6 @@ class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
+intents = discord.Intents.default()
+intents.message_content = True
+
