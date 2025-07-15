d913f7f78bf653a759c8c11865e516281d16707a
giannag27 <giacosey28@gmail.com>
2025-07-15 10:13:19 -0400

    logged in sucessfully

--- butter-square/butterbot.py
+++ butter-square/butterbot.py
@@ -9,6 +9,7 @@ class MyClient(discord.Client):
 intents = discord.Intents.default()
 intents.message_content = True
 
+load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = MyClient(intents=intents)
