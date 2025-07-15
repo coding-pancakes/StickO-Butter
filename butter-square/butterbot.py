f314116b6c0c2fc14fa5ea44a1da7506b4394878
giannag27 <giacosey28@gmail.com>
2025-07-15 09:54:29 -0400

    message abilites added

--- butter-square/butterbot.py
+++ butter-square/butterbot.py
@@ -13,3 +13,9 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = MyClient(intents=intents)
 client.run(TOKEN)
+
+async def on_message(self, message):
+    if message.author == self.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello World!')
